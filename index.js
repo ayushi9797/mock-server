52717b551c529eda8f1adf0940ae62dda3835192
ayushi9797 <soniayushi345@gmail.com>
2023-04-21 02:12:11 +0530

    mock-server

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.port || 4000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  console.log(`JSON Server is running ${port}`);
+});
